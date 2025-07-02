d0cb25f75917544ebb6b48c6caa8a73e2c11cc24
65030034 <65030034@kmitl.ac.th>
2025-07-03 02:03:05 +0700

    แก้ไข path รูปภาพให้สัมพันธ์กับ homepage

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@
 function Header() {
   return (
     <header className="header-container">
-      <img src={`${process.env.PUBLIC_URL}/picture/profile.jpg`} alt="Profile" />
+      <img src={`${process.env.PUBLIC_URL}/picture/Profile_picture.jpg`} alt="Profile" />
       <div className="text-content">
         <h1>Jirayut Padpol</h1>
         <p>Full-Stack Developer</p>
