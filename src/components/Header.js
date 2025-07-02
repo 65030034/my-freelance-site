f4f977fdc957e897fabecce98d27991d1174ce00
65030034 <65030034@kmitl.ac.th>
2025-07-03 01:57:57 +0700

    Fix duplicate project title in README

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@
 function Header() {
   return (
     <header className="header-container">
-      <img src="/picture/Profile_picture.jpg" alt="Profile" className="profile-img" />
+      <img src={`${process.env.PUBLIC_URL}/picture/profile.jpg`} alt="Profile" />
       <div className="text-content">
         <h1>Jirayut Padpol</h1>
         <p>Full-Stack Developer</p>
