4e2f6c73809a8fecd84b3acd0f2ffb459c15552a
65030034 <65030034@kmitl.ac.th>
2025-07-08 00:01:12 +0700

    fix PDF

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,21 @@
-
 function Header() {
   return (
     <header className="header-container">
-      <img src={`${process.env.PUBLIC_URL}/picture/profile_picture.jpg`} alt="Profile" className="profile-img" />
+      <img
+        src={`${process.env.PUBLIC_URL}/picture/profile_picture.jpg`}
+        alt="Profile"
+        className="profile-img"
+      />
       <div className="text-content">
         <h1>Jirayut Padpol</h1>
         <p>Full-Stack Developer</p>
+        <a
+          href={`${process.env.PUBLIC_URL}/PDF/Jirayut_Resume.pdf`}
+          download
+          className="download-btn"
+        >
+          Download Resume (PDF)
+        </a>
       </div>
     </header>
   );
